8e237fab3e474fb8905fe22704f38cdbd1c72ffb
Ravi Pratihast <ravipratihast@gmail.com>
2024-07-06 15:25:35 +0530

    Add: pages are mounted on the app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { Header } from "./Component/component-index";
+import { Routes, Route } from "react-router-dom";
+import { Product, Wishlist, Cart } from "./Pages/pages-index";
 const App = () => {
   return (
-    <>
+    <div className="">
       <Header />
-    </>
+
+      <Routes>
+        <Route path="/" element={<Product />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
   );
 };
 
