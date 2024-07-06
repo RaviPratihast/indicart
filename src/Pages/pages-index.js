c62e47cadcb6df22156f450d34398aed5dee0130
Ravi Pratihast <ravipratihast@gmail.com>
2024-07-06 15:24:57 +0530

    Add: pages product,wishlist,cart.

---
+++ src/Pages/pages-index.js
@@ -0,0 +1,5 @@
+import { Product } from "./Products-page/Product-page";
+import { Wishlist } from "./Wishlist-page/Wishlist-page";
+import { Cart } from "./Cart-page/Cart-page";
+
+export { Product, Wishlist, Cart };
