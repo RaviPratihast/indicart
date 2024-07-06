b18a96afc489a290825470d348e54849fc919e87
Ravi Pratihast <ravipratihast@gmail.com>
2024-07-06 11:31:20 +0530

    add: context added.

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ProductProvider } from "./Context/ProductContext/ProductContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <App />
+    <ProductProvider>
+      <App />
+    </ProductProvider>
   </Router>
 );
